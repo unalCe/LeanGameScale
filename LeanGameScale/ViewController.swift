796f382b48a427becbabe5f30f64618e443b4336
Ünal Çelik <uunal.celik@gmail.com>
2020-07-31 22:10:20 +0300

    Initial Commit

---
+++ LeanGameScale/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LeanGameScale
+//
+//  Created by Unal Celik on 31.07.2020.
+//  Copyright © 2020 unalCe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
