bbcd9dc6c2c5aeeff35c5fe51abd0e19b2f4ea16
Ünal Çelik <uunal.celik@gmail.com>
2020-08-02 23:21:05 +0300

    Kingfisher pod added.

---
+++ Pods/Target Support Files/Pods-LeanGameScale-LeanGameScaleUITests/Pods-LeanGameScale-LeanGameScaleUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LeanGameScale_LeanGameScaleUITests : NSObject
+@end
+@implementation PodsDummy_Pods_LeanGameScale_LeanGameScaleUITests
+@end
---
+++ Pods/Target Support Files/Pods-LeanGameScale/Pods-LeanGameScale-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LeanGameScale : NSObject
+@end
+@implementation PodsDummy_Pods_LeanGameScale
+@end
