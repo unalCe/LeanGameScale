42cdcd528cd126d3e5fbe66468e1831f6fb4b0a3
Ünal Çelik <uunal.celik@gmail.com>
2020-08-08 15:14:19 +0300

    TableViewLayout warning fix

--- LeanGameScale/Tabs/Games/GamesViewController.swift
+++ LeanGameScale/Tabs/Games/GamesViewController.swift
@@ -33,8 +33,8 @@ class GamesViewController: UIViewController, Storyboarded {
         setupTableView()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         
         updatePopBackCellsIsOpenedColor()
     }
